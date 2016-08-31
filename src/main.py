b34764a320ec54d5f4b62d2be275124958aa8639
212563094 <joshua.thornton@ge.com>
2016-08-30 22:51:38 -0400

    Built basic site and port test.

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import socket
+
+test_site = 'www.google.com'
+test_port = 80
+
+
+def service_exists(host, port):
+    try:
+        host_addr = socket.gethostbyname(host)
+        captive_dns_addr = socket.gethostbyname("FalseDomainThatDoesntExist.com")
+        if captive_dns_addr == host_addr:
+            return False
+    except:
+        pass
+
+    try:
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect((host, port))
+        s.close()
+    except:
+        return False
+
+    return True
+
+service_exists(test_site, test_port)
